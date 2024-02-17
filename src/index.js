c0d2c7320e8d352739f7fc3a40494ba44f5e365a
BitedRadish <a980831@naver.com>
2024-02-17 19:19:50 +0900

    togather frontend initialize

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h1>Togather FrontEnd 초기 상태입니다.</h1>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
