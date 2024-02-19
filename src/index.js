c78676380158a07a64a265353497a5f691ef1016
hjthebunny <monicahyunji@gmail.com>
2024-02-19 21:21:30 +0900

    Design: style initialization
    스타일 초기화, 전역 스타일 적용
    - body padding: 4vw
    - 폰트 기본 크기: 16px

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import GlobalStyles from "./styles/GlobalStyles.styles";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <h1>Togather FrontEnd 초기 상태입니다.</h1>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <GlobalStyles />
+    <h1>Togather FrontEnd 초기 상태입니다.</h1>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
---
+++ src/styles/GlobalStyles.styles.js
@@ -0,0 +1,58 @@
+import { Global, css } from "@emotion/react";
+
+const globalStyles = css`
+  //전역 스타일 설정
+  body {
+    font-family: Arial, sans-serif;
+    box-sizing: border-box;
+    padding: 0 4vw;
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  div,
+  span,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  a,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  form,
+  label,
+  table {
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+    vertical-align: baseline;
+  }
+
+  body {
+    line-height: 1;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+const GlobalStyles = () => <Global styles={globalStyles} />;
+
+export default GlobalStyles;
