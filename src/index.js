b3e6a8104d8d8b4f0189bc68466118e5020ce03b
hjthebunny <monicahyunji@gmail.com>
2024-02-21 02:15:58 +0900

    Feat: Header 구현
    (로고 적용 전)
    -  (비로그인) 숙소 등록, 로그인, 회원가입 버튼
    - (로그인) 숙소등록 버튼 ,프사, 이름, 알림 아이콘
    - 호버 시 스타일 적용

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import GlobalStyles from "./styles/GlobalStyles.styles";
 import RoomDetail from "./pages/RoomDetail";
+import Header from "./components/Header";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     <RoomDetail />
+    <Header />
   </React.StrictMode>,
   document.getElementById("root")
 );
