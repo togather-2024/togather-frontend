48687a1ef2361ffbf2fb816071958af29c1e6ee8
BitedRadish <a980831@naver.com>
2024-02-22 21:07:34 +0900

    MainSearch , MainCategory
    메인 페이지 내 MainSearch , MainCategory 초안 구현

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import GlobalStyles from "./styles/GlobalStyles.styles";
+import MainPage from "./pages/MainPage";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <h1>Togather FrontEnd 초기 상태입니다.</h1>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <GlobalStyles />
+        <MainPage></MainPage>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
