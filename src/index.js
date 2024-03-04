29fe3e349f5fd23166897590b3698c689385e9a6
hjthebunny <monicahyunji@gmail.com>
2024-03-04 18:59:50 +0900

    [feat] 이미지 컨테이너 ui 구현

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import GlobalStyles from "./styles/GlobalStyles.styles";
 import RoomDetail from "./pages/RoomDetail";
 import Header from "./components/Header";
+import RoomDetail from "./pages/RoomDetail";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     <RoomDetail />
     <Header />
+    <main>
+      <RoomDetail />
+    </main>
   </React.StrictMode>,
   document.getElementById("root")
 );
