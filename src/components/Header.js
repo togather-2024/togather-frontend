7b7d2ebd3f27d0bf9aa6a1797c4542395b4f9357
hjthebunny <monicahyunji@gmail.com>
2024-03-04 13:04:31 +0900

    [refactor] Apply color variable
    - 주석 제거
    - 색상 변수 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import styled from "@emotion/styled";
 import { CgProfile } from "react-icons/cg";
 import { FaBell } from "react-icons/fa";
+import { colors } from "../styles/colors";
+import { size } from "../styles/fonts";
 
 const Block = styled.header`
-  display: flex; /* flexbox 사용 */
+  display: flex;
   align-items: center;
-  border-bottom: 1px solid #d2d2d2;
+  border-bottom: 1px solid ${colors.gray10};
   height: 7vh;
   width: 100%;
 `;
@@ -14,20 +16,18 @@ const RightDiv = styled.div`
   display: flex;
   gap: 10px;
   flex-direction: row;
-  /* border: 1px solid black; */
   margin-left: auto;
 `;
 const Menu = styled.div`
   display: flex;
   gap: 6px;
-  color: #333333;
+  color: ${colors.black};
   border-radius: 10px;
-  font-size: 1rem;
+  font-size: ${size.body01};
   padding: 10px;
-  /* border: 1px solid red; */
   cursor: pointer;
   &:hover {
-    background-color: rgba(188, 239, 123, 0.5);
+    background-color: ${colors.hover01};
   }
 `;
 
