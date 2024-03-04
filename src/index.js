52f726590fa28231113c84917cb7d8e1fab6461e
hjthebunny <monicahyunji@gmail.com>
2024-03-04 18:55:38 +0900

    이미지 컨테이너 ui 구현

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Routing from "./routes/Routing";
 import GlobalStyles from "./styles/GlobalStyles.styles";
-import Header from "./components/Header";
-import RoomDetail from "./pages/RoomDetail";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Header />
-    <RoomDetail />
+    <GlobalStyles></GlobalStyles>
+    <BrowserRouter>
+      <Routing />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -3,24 +3,26 @@ import Header from "../components/Header/Header";
 import MainPage from "../pages/MainPage";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
+import RoomDetail from "../pages/RoomDetail";
 
 const Routing = () => {
-    return (
-        <>
-            <Routes>
-                <Route element={<Header />}>
-                    <Route index element={<MainPage />}></Route>
-                    <Route path="/signup" element={<SignUp />}></Route>
-                    <Route path="/signin" element={<SignIn />}></Route>
+  return (
+    <>
+      <Routes>
+        <Route element={<Header />}>
+          <Route index element={<MainPage />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/detail" element={<RoomDetail />}></Route>
 
-                    {/* 숙소 정보 페이지 */}
-                    {/* 예약 페이지 */}
-                    {/* 예약 확인 페이지 */}
-                    {/* 내 정보 페이지 */}
-                </Route>
-            </Routes>
-        </>
-    );
+          {/* 숙소 정보 페이지 */}
+          {/* 예약 페이지 */}
+          {/* 예약 확인 페이지 */}
+          {/* 내 정보 페이지 */}
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default Routing;
