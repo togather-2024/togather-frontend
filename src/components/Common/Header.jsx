96894f2893d49e278429ce1ba131e275672762d6
hjthebunny <monicahyunji@gmail.com>
2024-03-06 01:05:56 +0900

    [fix] 헤더 파일 수정 / 충돌 해결

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,15 +1,15 @@
 import styled from "@emotion/styled";
+import { Outlet } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { FaBell } from "react-icons/fa";
-import { colors } from "../styles/colors";
-import { size } from "../styles/fonts";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   const login = false;
   return (
     <>
       <Block>
-        <Logo src="./images/logo.png" alt="logo" />
+        <Logo src={logo} alt="logo" />
         {!login ? (
           <RightDiv>
             <Menu>숙소 등록</Menu>
@@ -28,6 +28,9 @@ const Header = () => {
           </RightDiv>
         )}
       </Block>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 };
@@ -35,33 +38,33 @@ const Header = () => {
 export default Header;
 
 const Block = styled.header`
-  display: flex;
+  display: flex; /* flexbox 사용 */
   align-items: center;
-  border-bottom: 1px solid ${colors.gray10};
+  border-bottom: 1px solid #d2d2d2;
   height: 7vh;
   width: 100%;
 `;
-
+const Logo = styled.img`
+  height: 50%;
+  cursor: pointer;
+`;
 const RightDiv = styled.div`
   display: flex;
   gap: 10px;
   flex-direction: row;
+  /* border: 1px solid black; */
   margin-left: auto;
 `;
 const Menu = styled.div`
   display: flex;
   gap: 6px;
-  color: ${colors.black};
+  color: #333333;
   border-radius: 10px;
-  font-size: ${size.body01};
+  font-size: 1rem;
   padding: 10px;
+  /* border: 1px solid red; */
   cursor: pointer;
   &:hover {
-    background-color: ${colors.hover01};
+    background-color: rgba(188, 239, 123, 0.5);
   }
-`;
-
-const Logo = styled.img`
-  height: 50%;
-  cursor: pointer;
 `;
--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Header from "../components/Header/Header";
+import Header from "../components/Common/Header";
 import MainPage from "../pages/MainPage";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
