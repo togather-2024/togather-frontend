59432cb98fc961b28b8bc7c47b493f466d6c1c7a
hjthebunny <monicahyunji@gmail.com>
2024-03-13 03:29:37 +0900

    [feat] CancelModal 구현
    - 모달 열릴 때 부모 컴포넌트 스크롤방지
    - 애니메이션 적용

--- src/components/ReservationDetail/CancelModal.jsx
+++ src/components/ReservationDetail/CancelModal.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { colors } from "../../styles/colors";
-import { size } from "../../styles/fonts";
+import { keyframes } from "@emotion/react";
 
-const CancelModal = () => {
+const CancelModal = ({ setIsOpen }) => {
+  function handleClose() {
+    setIsOpen(false);
+  }
   return (
     <ModalBackdrop>
       <Container>
         <ConfirmText>예약을 취소하시겠습니까?</ConfirmText>
         <BtnWrapper>
           <YesBtn>확인</YesBtn>
-          <NoBtn>취소</NoBtn>
+          <NoBtn onClick={handleClose}>취소</NoBtn>
         </BtnWrapper>
       </Container>
     </ModalBackdrop>
@@ -18,17 +21,27 @@ const CancelModal = () => {
 };
 
 export default CancelModal;
-//팝업 열었을때 페이지(!==모달) 컨테이너에 overflow:hidden 적용
+
+const fadeIn = keyframes`
+    0%{
+        opacity: 0;
+    }
+    100%{
+        opacity: 1;
+    }
+`;
+
 const ModalBackdrop = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: rgb(0, 0, 0, 0.7);
+  background-color: rgba(0, 0, 0, 0.7);
   display: flex;
   justify-content: center;
   align-items: center;
+  animation: ${fadeIn} ease-in-out 0.2s;
 `;
 const Container = styled.div`
   background-color: ${colors.white};
