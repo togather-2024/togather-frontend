616c9c3ff949cbfce4eb36372d4aa4a7e7461392
hjthebunny <monicahyunji@gmail.com>
2024-03-13 11:28:50 +0900

    [feat] 헤더에 링크 태크 적용
    - 주소 등록된 페이지 우선적으로 링크 태그 적용
    - 아직 등록되지 않은 주소들도 등록된 이후 적용 예정
    - 링크 밑줄 제거 (전역 스타일 설정)

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -7,74 +7,74 @@ import { colors } from "../../styles/colors";
 import { useRecoilState } from "recoil";
 import { loginState } from "../../recoil/atoms/loginState";
 const Header = () => {
-    const [isLoggedIn, setIsLoggedIn] = useRecoilState(loginState);
-    return (
-        <Block>
-            <Link to={`/`}>
-                <Logo src={logo} alt="logo" />
-            </Link>
-            {!isLoggedIn ? (
-                <RightDiv>
-                    {/* 추후 주소 변경 */}
-                    <Link to={`/registration`}>
-                        <Menu>숙소 등록</Menu>
-                    </Link>
-                    <Link to={`/signin`}>
-                        <Menu>로그인</Menu>
-                    </Link>
-                    <Link to={`/signup`}>
-                        <Menu>회원가입</Menu>
-                    </Link>
-                </RightDiv>
-            ) : (
-                <RightDiv>
-                    <Menu>숙소 등록</Menu>
-                    <Menu>
-                        <CgProfile /> 000 님
-                    </Menu>
-                    <Menu>
-                        <FaBell />
-                    </Menu>
-                </RightDiv>
-            )}
-        </Block>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(loginState);
+  return (
+    <Block>
+      <Link to={`/`}>
+        <Logo src={logo} alt="logo" />
+      </Link>
+      {!isLoggedIn ? (
+        <RightDiv>
+          {/* 추후 주소 변경 */}
+          <Link to={`/registration`}>
+            <Menu>숙소 등록</Menu>
+          </Link>
+          <Link to={`/signin`}>
+            <Menu>로그인</Menu>
+          </Link>
+          <Link to={`/signup`}>
+            <Menu>회원가입</Menu>
+          </Link>
+        </RightDiv>
+      ) : (
+        <RightDiv>
+          <Menu>숙소 등록</Menu>
+          <Menu>
+            <CgProfile /> 000 님
+          </Menu>
+          <Menu>
+            <FaBell />
+          </Menu>
+        </RightDiv>
+      )}
+    </Block>
+  );
 };
 
 export default Header;
 
 const Block = styled.header`
-    background-color: ${colors.white};
-    display: flex; /* flexbox 사용 */
-    align-items: center;
-    /* border-bottom: 1px solid #d2d2d2; */
-    height: 7vh;
-    width: 100%;
-    position: sticky;
-    top: 0;
-    z-index: 1;
+  background-color: ${colors.white};
+  display: flex; /* flexbox 사용 */
+  align-items: center;
+  /* border-bottom: 1px solid #d2d2d2; */
+  height: 7vh;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 1;
 `;
 const Logo = styled.img`
-    height: 30px;
-    cursor: pointer;
+  height: 30px;
+  cursor: pointer;
 `;
 const RightDiv = styled.div`
-    display: flex;
-    gap: 10px;
-    flex-direction: row;
-    /* border: 1px solid black; */
-    margin-left: auto;
+  display: flex;
+  gap: 10px;
+  flex-direction: row;
+  /* border: 1px solid black; */
+  margin-left: auto;
 `;
 const Menu = styled.div`
-    display: flex;
-    gap: 6px;
-    color: #333333;
-    border-radius: 10px;
-    font-size: 1rem;
-    padding: 10px;
-    /* border: 1px solid red; */
-    cursor: pointer;
-    &:hover {
-        background-color: rgba(188, 239, 123, 0.5);
-    }
+  display: flex;
+  gap: 6px;
+  color: #333333;
+  border-radius: 10px;
+  font-size: 1rem;
+  padding: 10px;
+  /* border: 1px solid red; */
+  cursor: pointer;
+  &:hover {
+    background-color: rgba(188, 239, 123, 0.5);
+  }
 `;
