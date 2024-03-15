d1072e8470b4c2391b2915e3ec41f9ca5a69bcb7
hjthebunny <monicahyunji@gmail.com>
2024-03-15 22:20:31 +0900

    [feat] Outlet 적용 스타일 수정

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,23 +1,24 @@
 import { Route, Routes } from "react-router-dom";
-import Header from "../components/Common/Header";
 import MainPage from "../pages/MainPage";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
 import RoomDetail from "../pages/RoomDetail";
 import ReservationConfirm from "../pages/ReservationConfirm";
+import Layout from "../components/Common/Layout";
 
 const Routing = () => {
   return (
     <>
-      <Header />
       <Routes>
-        <Route index element={<MainPage />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/signin" element={<SignIn />}></Route>
-        <Route path="/num/detail" element={<RoomDetail />}></Route>
-        <Route path="/num/reservate" element={<ReservationConfirm />}></Route>
-        {/* 예약 확인 페이지 */}
-        {/* 내 정보 페이지 */}
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<MainPage />}></Route>
+          <Route path="signup" element={<SignUp />}></Route>
+          <Route path="signin" element={<SignIn />}></Route>
+          <Route path="/num/detail" element={<RoomDetail />}></Route>
+          <Route path="/num/reservate" element={<ReservationConfirm />}></Route>
+          {/* 예약 확인 페이지 */}
+          {/* 내 정보 페이지 */}
+        </Route>
       </Routes>
     </>
   );
