aa85498add937a4fe568e89278ac84a2f4e7ac65
BitedRadish <a980831@naver.com>
2024-03-25 21:54:20 +0900

    0325, pre commit

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -52,18 +52,14 @@ const Header = () => {
 export default Header;
 
 const Block = styled.header`
-  background-color: ${colors.white};
   display: flex; /* flexbox 사용 */
   align-items: center;
-  /* border-bottom: 1px solid #d2d2d2; */
+  border-bottom: 1px solid #d2d2d2;
   height: 7vh;
   width: 100%;
-  position: sticky;
-  top: 0;
-  z-index: 1;
 `;
 const Logo = styled.img`
-  height: 30px;
+  height: 50%;
   cursor: pointer;
   height: 30px;
   cursor: pointer;
