3882675154e160f44652a43d79de0f1bf3d09318
hjthebunny <monicahyunji@gmail.com>
2024-03-27 03:43:01 +0900

    [design] 메인페이지 여백 조정 및 ui 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -73,40 +73,7 @@ const MainPage = () => {
     };
     fetchPhotos();
   }, [photos]);
-  return (
-    <>
-      {/* profile dropdown은 Link to */}
-      <Container>
-        <MainSearch></MainSearch>
-        <MainCategory keywords={keywords}></MainCategory>
-        {/* 카테고리 값과 , 검색 결과 변수 바뀔 때마다 Contents 내용 변경 */}
-        {/* idx변수는 roomId로 변경 필요 */}
-        <MainContents>
-          {photos &&
-            photos.map((photo, idx) => (
-              <Link to={`/detail/${idx + 1}`}>
-                <Card key={idx} photo={photo}></Card>
-              </Link>
-            ))}
-          {/* {isLoading && <p>Loading</p>} */}
-        </MainContents>
-      </Container>
-    </>
-  );
-        // for (let i = 0; i < arrSize; i++) {
-        //     await setPhotos((prev) => [
-        //         ...prev,
-        //         data.slice(i, (i + 1) * 100 + 1),
-        //     ]);
-        // }
-        // await setExp(photos[page.current]);
-        setPhotos(data);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    fetchPhotos();
-  }, [photos]);
+
   return (
     <>
       {/* profile dropdown은 Link to */}
