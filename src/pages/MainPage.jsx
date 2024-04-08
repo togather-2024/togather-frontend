ea94879dd8cd9f44595a6a274a437a776ee279fe
BitedRadish <48427105+BitedRadish@users.noreply.github.com>
2024-04-08 23:00:57 +0900

    Update MainPage.jsx
    업데이트 이전 부분 삭제

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,112 +4,71 @@ import axios from "axios";
 import MainSearch from "../components/MainPage/MainSearch";
 import MainCategory from "../components/MainPage/MainCategory";
 import Card from "../components/MainPage/MainContents/Card";
-import { Link } from "react-router-dom";
-
-// import useIntersect from "../hooks/useIntersect";
 
 const MainPage = () => {
-  // const dispatch = useDispatch();
+    const [keywords, setKeywords] = useState([]);
+    const [photos, setPhotos] = useState([]);
 
-  // const { apiData, isLoaded, pageCount } = useSelector((state) => ({
-  //     apiData: state.apiData.data,
-  //     isLoaded: state.apiData.isLoaded,
-  //     pageCount: state.pageReducer.pageCount,
-  // }));
+    useEffect(() => {
+        const fetchKeyword = async () => {
+            try {
+                const response = await axios.get(
+                    "https://jsonplaceholder.typicode.com/users/"
+                );
+                setKeywords(response.data);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchKeyword();
+    }, [keywords]);
 
-  const [keywords, setKeywords] = useState([]);
-  const [photos, setPhotos] = useState([]);
-  // const [exp, setExp] = useState();
+    useEffect(() => {
+        const fetchPhotos = async () => {
+            try {
+                const response = await axios.get(
+                    "https://jsonplaceholder.typicode.com/photos/"
+                );
+                const data = await response.data
+                    .filter((_, idx) => idx < 42)
+                    .map((el) => el.thumbnailUrl);
 
-  // const page = useRef(0);
-
-  // const [_, setRef] = useIntersect(
-  //     async (entry, observer) => {
-  //         observer.unobserve(entry.target);
-  //         await setExp(photos[++page.current]);
-  //         observer.observe(entry.target);
-  //     },
-  //     [exp]
-  // );
-
-  // // const [page, setPage] = useState(1);
-  // const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchKeyword = async () => {
-      try {
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/users/"
-        );
-        setKeywords(response.data);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    fetchKeyword();
-  }, [keywords]);
-
-  useEffect(() => {
-    const fetchPhotos = async () => {
-      try {
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/photos/"
-        );
-        const data = await response.data
-          .filter((_, idx) => idx < 42)
-          .map((el) => el.thumbnailUrl);
-        // const arrSize = (await data.length) / 100;
-
-        // for (let i = 0; i < arrSize; i++) {
-        //     await setPhotos((prev) => [
-        //         ...prev,
-        //         data.slice(i, (i + 1) * 100 + 1),
-        //     ]);
-        // }
-        // await setExp(photos[page.current]);
-        setPhotos(data);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    fetchPhotos();
-  }, [photos]);
-
-  return (
-    <>
-      {/* profile dropdown은 Link to */}
-      <Container>
-        <MainSearch></MainSearch>
-        <MainCategory keywords={keywords}></MainCategory>
-        {/* 카테고리 값과 , 검색 결과 변수 바뀔 때마다 Contents 내용 변경 */}
-
-        {/* idx변수는 roomId로 변경 필요 */}
-        <MainContents>
-          {photos &&
-            photos.map((photo, idx) => (
-              <Link to={`/${idx + 1}/detail`}>
-                <Card key={idx} photo={photo}></Card>
-              </Link>
-            ))}
-          {/* {isLoading && <p>Loading</p>} */}
-        </MainContents>
-      </Container>
-    </>
-  );
+                setPhotos(data);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchPhotos();
+    }, [photos]);
+    return (
+        <>
+            <Container>
+                <MainSearch></MainSearch>
+                <MainCategory keywords={keywords}></MainCategory>
+                <MainContents>
+                    {photos &&
+                        photos.map((photo, idx) => (
+                            <Card key={idx} photo={photo}></Card>
+                        ))}
+                </MainContents>
+            </Container>
+        </>
+    );
 };
 
 export default MainPage;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  overflow-y: auto;
-  height: calc(100vh-64px);
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    overflow-y: auto;
+    height: calc(100vh-64px);
 `;
 
 const MainContents = styled.div`
-  width: 92vw;
-  display: flex;
-  gap: 0px 3.6rem;
-  flex-wrap: wrap;
+    width: 92vw;
+    display: flex;
+    gap: 0px 3.6rem;
+    flex-wrap: wrap;
 `;
